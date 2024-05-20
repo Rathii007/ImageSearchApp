d44b688f14644bc22815551bd87295586f63d094
Mayank Rathi <104123897+Rathii007@users.noreply.github.com>
2024-05-20 16:46:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const inputElement = document.getElementById("search-input");
 const searchResults = document.querySelector(".search-results");
 const showMoreButton = document.getElementById("show-more-button");
 
-//used darkmode.js to implement the dark mode toggle button
 const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.5s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true
-}
+  bottom: "unset",
+  right: "32px",
+  left: "unset",
+  top: "32px",
+  time: "0.5s",
+  mixColor: "#fff",
+  backgroundColor: "#fff",
+  buttonColorDark: "#100f2c",
+  buttonColorLight: "#fff",
+  saveInCookies: false,
+  label: "🌓",
+  autoMatchOsTheme: true,
+};
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
@@ -26,50 +26,57 @@ let inputData = "";
 let page = 1;
 
 async function searchImages() {
-    inputData = inputElement.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  inputData = inputElement.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    const results = data.results;
+  const results = data.results;
 
-    if (page === 1) {
-        searchResults.innerHTML = "";
-    }
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
 
-    results.map((result) => {
-        const imageWrapper = document.createElement("div");
-        imageWrapper.classList.add("search-result");
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        image.alt = result.alt_description;
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
-        imageLink.textContent = result.alt_description;
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
 
-        imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        searchResults.appendChild(imageWrapper);
-    });
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
 
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
 
-    if (page < data.total_pages) {
-        showMoreButton.style.display = "block";
-    } else {
-        showMoreButton.style.display = "none";
-    }
+    imageLink.appendChild(image);
+    imageWrapper.appendChild(imageLink);
 
-    page++;
+    const imageDescription = document.createElement("a");
+    imageDescription.href = result.links.html;
+    imageDescription.target = "_blank";
+    imageDescription.textContent = result.alt_description;
+
+    imageWrapper.appendChild(imageDescription);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  if (page < data.total_pages) {
+    showMoreButton.style.display = "block";
+  } else {
+    showMoreButton.style.display = "none";
+  }
+
+  page++;
 }
 
 formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    page = 1;
-    searchImages();
+  event.preventDefault();
+  page = 1;
+  searchImages();
 });
 
 showMoreButton.addEventListener("click", () => {
-    searchImages();
+  searchImages();
 });
